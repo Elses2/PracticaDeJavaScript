e95d24b9d600c2cf6d2bab8407eae0dca0d205f4
eber <eberchiecher2@gmail.com>
2022-06-07 19:36:28 -0300

    primer commit

---
+++ src/main/java/com/mycompany/tablahash/Main.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.tablahash;
+
+/**
+ *
+ * @author elses
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
